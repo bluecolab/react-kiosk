5fa829056ddf7e199a44d6c129e77e3252a220d9
Kenji Okura <okurakeng@gmail.com>
2025-04-03 09:45:38 -0400

    [Rename] Re-name most of the icons, delete unused assets (#11)

--- src/types/media.d.ts
+++ src/types/media.d.ts
@@ -1,3 +1,7 @@
+/** by default .js/jsx/.ts/.tsx and image files are known to typescript
+ *  But files like .mp4s .webms and .pdfs need to be declared as below 
+ *  for typescript to 'know' the extension
+ */
 declare module "*.mp4" {
     const src: string;
     export default src;
