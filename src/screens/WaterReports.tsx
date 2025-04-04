27fd519ff1404bf48fd71fa51e5f924559453d4f
Kenji Okura <okurakeng@gmail.com>
2025-04-04 12:01:25 -0400

    [Fix] Fix imports (#14)

--- src/screens/WaterReports.tsx
+++ src/screens/WaterReports.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import * as React from 'react';
 import { waterReports } from '../components/WaterReportFiles'; // Where all the files are stored
-
+import NotFound from '../components/Error404';
 export default function WaterReport() {
   return (
     <Box sx={{ textAlign: 'center', p: 2 }}>
