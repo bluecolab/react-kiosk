227e4a2267152ca7e08b53bd1562cb3dc43b9058
Kenji Okura <okurakeng@gmail.com>
2025-04-04 12:00:36 -0400

    [Fix] Fix imports

--- src/screens/MobileApp.tsx
+++ src/screens/MobileApp.tsx
@@ -2,7 +2,7 @@
 import { Box } from '@mui/material';
 import * as React from 'react';
 import Mobile from '../assets/icons/MobileNewUI.png'
-// Import for QR code still needed - V
+import NotFound from '../components/Error404';
 
 export default function MobileApp() {
     return (<Box sx={{ textAlign: 'center', p: 2 }}>
