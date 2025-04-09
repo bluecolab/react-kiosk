2d7a326db91b2b34fe7dc9413c6a57e7a3944b8d
Kenji Okura <okurakeng@gmail.com>
2025-04-08 23:48:08 -0400

    Revert "reactify-branch (#9)"
    This reverts commit 4d3ab4bc6dc0a78ad39cfb683e65ea63b492948c.

---
+++ src/screens/AboutUs.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@mui/material';
+import * as React from 'react';
+
+export default function AboutUs() {
+    return (<Box sx={{ textAlign: 'center', p: 2 }}>
+        <p>Learn more about Blue CoLab. Test edit to trigger a build.</p>
+    </Box>)
+}
---
+++ src/screens/DataToMusic.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+import * as React from 'react';
+
+
+
+export default function DataToMusic() {
+    return (<Box sx={{ textAlign: 'center', p: 2 }}>
+        <p>Convert water data into music.</p>
+    </Box>)
+}
