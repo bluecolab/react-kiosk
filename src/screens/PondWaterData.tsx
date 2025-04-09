4d3ab4bc6dc0a78ad39cfb683e65ea63b492948c
Kenji Okura <okurakeng@gmail.com>
2025-04-08 22:37:25 -0400

    reactify-branch (#9)

--- src/screens/PondWaterData.tsx
+++
@@ -1,8 +0,0 @@
-import Box from '@mui/material/Box';
-import * as React from 'react';
-
-export default function PondWaterData() {
-    return (<Box sx={{ textAlign: 'center', p: 2 }}>
-        <p>Monitor pond water quality with real-time data.</p>
-    </Box>)
-}
