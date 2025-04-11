742e89ff93bf530cb98a541e50220f35f1594ff1
ZVictorLima <victor.an.lima@hotmail.com>
2025-04-11 12:45:00 -0400

    Lil Mobile page update

--- screens/MobileApp.tsx
+++ screens/MobileApp.tsx
@@ -1,20 +1,24 @@
 // Yes Yes work in progress - V
 import React from 'react';
-import InProgress from '@/components/InProgress';
-import { View, Image, Text} from 'react-native';
+import { View, Image, Text } from 'react-native';
+import QRPlaceholder from '@/assets/images/QR Placeholder.png'
 
 export default function MobileApp() {
-    return (<View>
-        <div style={{ display: 'flex' }}>        
-            <Image source={require("../assets/images/icons/MobileNewUI.png")} alt="Mobile App"/>
-        </div>
-        <div>
-            <Text style={{fontSize: 20, fontWeight:'bold'}}>Mobile App</Text>
-            <Text>Access our research data and information on the go.</Text>
-            <Text>Stay updated with real-time notifications.</Text>
-            <Text>Download our mobile app for better access.</Text>
-        <InProgress />
-
-        </div>
-    </View>)
-}   
+    return (
+        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ flex: 1, alignItems: 'center' }}>
+                <Image 
+                    style={{ width: 300, height: 600 }} 
+                    source={require("../assets/images/icons/MobileNewUI.png")} 
+                    alt="Mobile App" 
+                />
+            </View>
+            <View style={{ flex: 1, flexDirection: 'column', alignItems: 'flex-start', paddingLeft: 20 }}>
+                <Text style = {{}}>Access our research data and information on the go.</Text>
+                <Text>Stay updated with real-time notifications.</Text>
+                <Text>Download our mobile app for better access.</Text>
+            </View>
+            {/* Gotta add the QR Place holder here somewhere for Apple and Android -V*/}
+        </View>
+    );
+}
