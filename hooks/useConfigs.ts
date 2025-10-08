b002251e91bbd0f59004fc98313daa453629f8d8
Kenji Okura <okurakeng@gmail.com>
2025-10-08 18:34:11 -0400

    Remove legacy code

--- hooks/useConfigs.ts
+++ hooks/useConfigs.ts
@@ -1,11 +1,8 @@
-// useKioskConstants.js - Manages application configurations based on user preferences, specifically for "Crotter Mode".
+// useKioskConstants.js - Manages application configurations based on user preferences depending on the UI type specified in the URL.
 
 import { constants, UIType } from './constants/constants';
 
 export const useConfigs = () => {
-    // constants.secret = localStorage.getItem('crotterMode') === 'true'; // Check if Crotter Mode is enabled
-    // Adjust view area height and carousel location based on Crotter Mode status
-
     const type = new URLSearchParams(window.location.search).get('uiType');
     switch (type) {
         case UIType.DOCK:
