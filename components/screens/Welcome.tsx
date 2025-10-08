c2625b9c07659e80365e0c19cbb7321cc368593a
Kenji Okura <okurakeng@gmail.com>
2025-10-08 18:35:20 -0400

    Mac like menu bar  (#35)
    * Clean up package-lock
    
    * Mac like bar to switch between screens
    
    * Fix spelling
    
    * Remove legacy code

--- components/screens/Welcome.tsx
+++ components/screens/Welcome.tsx
@@ -3,7 +3,6 @@ import { View, Text, Image } from 'react-native'; //, Pressable for future use o
 
 export default function WelcomeScreen() {
     let Cronin = require('@/assets/images/crotters/Crotter.png');
-    // let gif = require('@/assets/images/general/Scroll.gif');
 
     return (
         <View>
@@ -18,26 +17,7 @@ export default function WelcomeScreen() {
             </Text>
 
             <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
-                {/* <Pressable
-                    onPress={() => {
-                        console.log('Crotter Mode Enabled');
-                        localStorage.setItem('crotterMode', 'true'); // Enable Crotter Mode on press
-                        window.location.reload(); // Reload to apply changes
-                    }}> */}
-                <View
-                    style={{ height: 500, flex: 1 }}
-                    onStartShouldSetResponder={() => true}
-                    onResponderRelease={() => {
-                        if (localStorage.getItem('crotterMode') === 'true') {
-                            console.log('Crotter Mode Disabled :(');
-                            localStorage.setItem('crotterMode', 'false');
-                            window.location.reload();
-                        } else {
-                            console.log('Crotter Mode Enabled');
-                            localStorage.setItem('crotterMode', 'true');
-                            window.location.reload(); // Reload to apply changes
-                        }
-                    }}>
+                <View style={{ height: 500, flex: 1 }}>
                     <Image source={Cronin} style={{ height: 490, width: '100%' }} />
                 </View>
                 {/* </Pressable> */} {/* Left Side with Image */}
