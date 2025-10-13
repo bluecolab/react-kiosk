180aff89ca257ecc8445276db18865ecf1a3d8e9
Kenji Okura <okurakeng@gmail.com>
2025-10-13 15:16:10 -0400

    Disable pinch to zoom

--- app/index.tsx
+++ app/index.tsx
@@ -51,6 +51,10 @@ export default function Index() {
             <Head>
                 <title>Blue CoLab Kiosk</title>
                 <meta name="description" content="Blue CoLab Kiosk" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+                />
             </Head>
 
             <View style={{ flex: 1, position: 'relative' }}>
