7e5fd3d39be93458ac9a650cc37437054d8782d3
LloydDrums <lkbamoah@gmail.com>
2025-10-17 17:09:55 -0400

    Updated

--- components/screens/Welcome.tsx
+++ components/screens/Welcome.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import { View, Text, Image } from 'react-native'; //, Pressable for future use of Pressable component
 
 export default function WelcomeScreen() {
-    let Cronin = require('@/assets/images/general/fall2025team.jpg'); 
-  
- return ( 
-     <View> 
-         <Text 
-             style={{
-                 textAlign: 'center', 
-                 fontSize: 24, 
-                 fontWeight: 'bold', 
-                 marginVertical: 30, 
-             }}> 
-             Welcome to Blue CoLab's Kiosk!{' '} 
-         </Text> 
-  
-         <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}> 
-            <View style={{ height: 500, flex: 1 }}> 
-                <Image source={Cronin} style={{ height: 500, width: '100%'}} /> 
-            </View> 
-            {/* </Pressable> */} {/* Left Side with Image */} 
-         </View> 
-     </View>
+    let Cronin = require('@/assets/images/general/fall2025team.jpg');
+
+    return (
+        <View>
+            <Text
+                style={{
+                    textAlign: 'center',
+                    fontSize: 24,
+                    fontWeight: 'bold',
+                    marginVertical: 30,
+                }}>
+                Welcome to Blue CoLab's Kiosk!{' '}
+            </Text>
+
+            <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
+                <View style={{ height: 500, flex: 1 }}>
+                    <Image source={Cronin} style={{ height: 500, width: '100%' }} />
+                </View>
+                {/* </Pressable> */} {/* Left Side with Image */}
+            </View>
+        </View>
     );
 }
