e7e868d7ae58253f0e0e78b0b9db17b6c2172c82
Robert Bunjaj <rbujaj99@gmail.com>
2025-11-03 13:48:01 -0500

    Expand button Robert Bunjaj (Team Leader) (#72)
    * Enlarged the weather page and changed the background color to a Silvery color
    
    * Minor changes to AboutUs.tsx: aligned the two images parallel. Reworded some of the text (Can be reverted if not good). For ScrollingCarousel.tsx attempted to align the icons in center (A more beta look)
    
    * Added a "helpful error" to DataToMusic, will
    back if needed.
    
    * Added a helpful error/placeholder. Will be reverted back when needed too.
    
    * Added a placeholder for the DataToMusic as a helpful error.
    
    * Added a placeholder for the DataToMusic.tsx and made minor adjustments for it.
    
    * Made some minor changes to the AboutUs.tsx including the images being aligned and made the words slightly bigger for better readability, (if it is not good or any different we can leave it).
    
    * Created a Feedback Button in the welcome.tsx
    
    * Changed the layout of the Water Reports page.
    
    * Updated the Water Reports page.
    
    * Added Feedback Viewer and a Local Storage so it saves all the feedback
    
    * Added a Feedback Viewer for saved responses.
    
    * Added a Feedback Button, with a Feedback Viewer that'll save the responses.
    
    * Very minimum changes. Added a Feedback Storage
    
    * Feedback Button added with saved responses and updated the Water Reports page
    
    * Added a "bottom" code and adjusted it to "15" It should be a perfect fit and clickable.
    
    ---------
    
    Co-authored-by: Kenji Okura <okurakeng@gmail.com>

--- components/wrapper/ExpandButton.tsx
+++ components/wrapper/ExpandButton.tsx
@@ -13,6 +13,10 @@ export default function ExpandButton({ isExpanded, onPress }: ExpandButtonProps)
                 justifyContent: 'center',
                 alignItems: 'center',
                 width: '100%',
+                marginBottom: 10,
+                borderTopColor: 'black',
+                borderTopWidth: 0,
+                bottom: 15,
             }}>
             <TouchableOpacity onPress={onPress}>
                 <Text
