5ef3e1004090dd1d772d76a9e7122f1bc6e78b40
Kenji Okura <okurakeng@gmail.com>
2025-11-19 02:47:01 -0500

    Fix bug

--- components/wrapper/ExpandButton.tsx
+++ components/wrapper/ExpandButton.tsx
@@ -27,7 +27,7 @@ export default function ExpandButton({ isExpanded, onPress }: ExpandButtonProps)
                         textAlign: 'center',
                         fontSize: 18,
                     }}>
-                    {isExpanded ? '△ Shrink △' : '▽ Expand on no this is a bug! ▽'}
+                    {isExpanded ? '△ Shrink △' : '▽ Expand ▽'}
                 </Text>
             </TouchableOpacity>
         </View>
