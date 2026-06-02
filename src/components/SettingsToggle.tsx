5deb68b9f7eeef717fa0b87969fd35d4dae64751
Kenji Okura <okurakeng@gmail.com>
2026-06-01 21:20:06 -0400

    Fix formatting

--- src/components/SettingsToggle.tsx
+++ src/components/SettingsToggle.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Pressable, Text, View, StyleSheet } from 'react-native';
 import i18n from '@/hooks/i18n';
-import Entypo from "@react-native-vector-icons/entypo";
+import Entypo from '@react-native-vector-icons/entypo';
 import { useColorScheme } from 'nativewind';
 import Animated, {
     withDelay,
